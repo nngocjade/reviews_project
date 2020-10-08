1fdb2b1b754f02c44aaeecd9eb045d40ea2018c6
nngocjade <nngocjade@gmail.com>
2020-10-08 13:01:56 +0800

    adding details

---
+++ dist/js/app.js
@@ -0,0 +1,36 @@
+//local reviews data
+const review = [
+  {
+    id: 1,
+    name: "Birdy",
+    job: "Fly",
+    img: "ClonedToGithubdistimg\bird-1045954_640.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur obcaecati voluptatum, quod tempore minus eos hic facere, optio odit cum saepe nulla ducimus, laborum error perspiciatis architecto distinctio aliquam doloremque.",
+  },
+  {
+    id: 2,
+    name: "Pelican",
+    job: "Hunt",
+    img: "ClonedToGithubdistimgpelican-5218855_640.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur obcaecati voluptatum, quod tempore minus eos hic facere, optio odit cum saepe nulla ducimus, laborum error perspiciatis architecto distinctio aliquam doloremque.",
+  },
+  {
+    id: 3,
+    name: "Kitty Kat",
+    job: "Purr",
+    img: "ClonedToGithubdistimgcat-5625168_640.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur obcaecati voluptatum, quod tempore minus eos hic facere, optio odit cum saepe nulla ducimus, laborum error perspiciatis architecto distinctio aliquam doloremque.",
+  },
+
+  {
+    id: 4,
+    name: "Migratory Birds",
+    job: "Dance in the Sky",
+    img: "ClonedToGithubdistimgmigratory-birds-5242969_640.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur obcaecati voluptatum, quod tempore minus eos hic facere, optio odit cum saepe nulla ducimus, laborum error perspiciatis architecto distinctio aliquam doloremque.",
+  },
+];
