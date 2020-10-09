fd98da9225b62491f31d91f1bf335f2922034d8c
nngocjade <nngocjade@gmail.com>
2020-10-10 04:11:15 +0800

    adding js code

--- dist/js/app.js
+++ dist/js/app.js
@@ -34,3 +34,21 @@ const review = [
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur obcaecati voluptatum, quod tempore minus eos hic facere, optio odit cum saepe nulla ducimus, laborum error perspiciatis architecto distinctio aliquam doloremque.",
   },
 ];
+
+// select items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+  console.log("shake and bake");
+});
